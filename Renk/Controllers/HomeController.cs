8859ee54cde18f8371fd753c09c811973ed70e86
gzm <gzm@DESKTOP-JPGL5QD>
2025-01-30 01:25:44 +0300

    Proje dosyası ekle.

---
+++ Renk/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Renk.Controllers;
+
+public class HomeController : Controller
+{
+    
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+  
+
+    
+}
